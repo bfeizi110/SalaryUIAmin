464e4be06257a8a1905b9f6f2c216ff9d688e4fe
Behzad <Behzad@MANAGER-DESKTOP>
2025-12-01 22:09:35 +0330

    Commit All

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { getBaseUrl } from '../app/app.module'
 
 export const environment = {
   production: true,
-   //API_URL: "http://192.168.1.10:1010/PayrollApi/api/v1/",
-  API_URL: `${getBaseUrl()}/payrollapi/api/v1/`,
+   API_URL: "http://192.168.1.10:1010/PayrollApi/api/v1/",
+  //API_URL: `${getBaseUrl()}/payrollapi/api/v1/`,
 
 };
